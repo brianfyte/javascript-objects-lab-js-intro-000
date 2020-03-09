eba66c091e4412bdc6f678245bed7f689ebbe87c
Brian Wong <brianw385@gmail.com>
2020-03-09 01:02:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(recipes, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) {
   return object.assign({}, object, {[key]: value});
 }
